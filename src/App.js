c0038487dd360a28b6bcfddcef2c5a6a8926854c
tzx4ys <otabek.matqulov@gmail.com>
2023-02-23 11:44:00 +0500

    added
    import NewTodoForm from './components/NewTodoForm';

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import TodoTable from './components/TodoTable';
+import NewTodoForm from './components/NewTodoForm';
 
 function App() {
 
